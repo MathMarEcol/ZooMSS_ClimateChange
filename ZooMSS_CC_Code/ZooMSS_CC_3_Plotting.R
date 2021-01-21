b309a8536ef53dd7bc2d8f3f9d5f9893f2f07c83
jaseeverett <Jason.Everett@unsw.edu.au>
2021-01-21 12:33:13 +1100

    Update figures

--- ZooMSS_CC_Code/ZooMSS_CC_3_Plotting.R
+++ ZooMSS_CC_Code/ZooMSS_CC_3_Plotting.R
@@ -3,51 +3,73 @@ library(patchwork)
 library(lubridate)
 
 base_dir <- file.path("~","Nextcloud","MME2Work","ZooMSS_ClimateChange","")
+reprocess <- TRUE
 
-runs <- c("Control", "NoCarnivores", "NoOmnivores", "NoFilterFeeders", "OneZoo")
-models <- c("UKESM1", "MPI", "IPSL", "GFDL", "CESM2")
+if (reprocess == TRUE){
+  ## Create a summary df to hold the spatial means of all the data
+  runs <- c("Control", "NoCarnivores", "NoOmnivores", "NoFilterFeeders", "OneZoo")
+  models <- c("UKESM1", "MPI", "IPSL", "GFDL", "CESM2")
 
-# #### Load ZooMSS Matrix Data ####
-# enviro_data <- read_rds("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/enviro_CMIP_Matrix_wPhyto.RDS")
+  for (m in 1:length(models)){
+    for (r in 1:length(runs)){
 
-datyr <- tibble(Date = ymd(), Experiment = character(), Model = character(), Run = character(), Fish = numeric(), BiomassChange = numeric())
+      dat <- read_rds(paste0(base_dir, "ClimateChange_Compiled_withZooMSS_",models[m],"_",runs[r],".rds"))
 
-for (m in 1:length(models)){
-  for (r in 1:length(runs)){
+      temp <- dat %>%
+        group_by(Date, Year, Experiment) %>%
+        summarise(across(Flagellates:Fish_Large, ~mean(.x, na.rm = TRUE))) %>%
+        ungroup() %>%
+        arrange(Date) %>%
+        mutate(Run = runs[r],
+               Model = models[m],
+               Fish_Total = Fish_Small + Fish_Med + Fish_Large) %>%
+        mutate(FishBiomassChange = (Fish_Total - mean(Fish_Total[Year<1960], na.rm = TRUE))/mean(Fish_Total[Year<1960], na.rm = TRUE) * 100)
 
-    dat <- read_rds(paste0(base_dir, "ClimateChange_Compiled_withZooMSS_",models[m],"_",runs[r],".rds"))
-
-    temp <- dat %>%
-      group_by(Date, Experiment) %>%
-      summarise(Fish = mean(Fish_Small + Fish_Med + Fish_Large)) %>%
-      ungroup() %>%
-      arrange(Date) %>%
-      mutate(Run = runs[r],
-             Model = models[m])
-
-    temp$BiomassChange = (temp$Fish - mean(temp$Fish[1:10], na.rm = TRUE))/mean(temp$Fish[1:10], na.rm = TRUE) * 100
-
-    datyr <- bind_rows(datyr, temp)
-
-    rm(temp, dat)
+      if (m == 1 & r == 1){
+        datyr <- temp} else{
+          datyr <- bind_rows(datyr, temp)
+        }
+      rm(temp, dat)
+    }
   }
+
+  write_rds(datyr, "ClimateChange_Compiled_withZooMSS_GlobalYr.rds")
+} else{
+  datyr <- read_rds("ClimateChange_Compiled_withZooMSS_GlobalYr.rds")
 }
 
-write_rds(datyr, paste0(base_dir, "ClimateChange_Compiled_withZooMSS_GlobalYr.rds"))
 
 graphics.off()
 x11(width = 16, height = 12)
-(ggB <- ggplot(datyr, aes(x = Date, y = Fish, colour = Experiment)) +
-  geom_line() +
-  theme_bw() +
-  facet_grid(vars(Run), vars(Model), scales = "free_y"))
+(ggB <- ggplot(datyr, aes(x = Date, y = Fish_Total, colour = Experiment)) +
+    geom_line() +
+    theme_bw() +
+    facet_grid(vars(Run), vars(Model), scales = "free_y"))
 ggsave("Figures/Fish_TotalBiomass.pdf")
 
+
 graphics.off()
 x11(width = 16, height = 12)
-(ggBC <- ggplot(datyr, aes(x = Date, y = BiomassChange, colour = Experiment)) +
-  geom_line() +
-  theme_bw() +
-  facet_grid(vars(Run), vars(Model), scales = "fixed"))
+(ggOC <- ggplot(datyr, aes(x = Date, y = OmniCopepods, colour = Experiment)) +
+    geom_line() +
+    theme_bw() +
+    facet_grid(vars(Run), vars(Model), scales = "free_y"))
+ggsave("Figures/OmniCopepods_TotalBiomass.pdf")
+
+graphics.off()
+x11(width = 16, height = 12)
+(ggCC <- ggplot(datyr, aes(x = Date, y = CarnCopepods, colour = Experiment)) +
+    geom_line() +
+    theme_bw() +
+    facet_grid(vars(Run), vars(Model), scales = "free_y"))
+ggsave("Figures/CarnCopepods_TotalBiomass.pdf")
+
+
+graphics.off()
+x11(width = 16, height = 12)
+(ggBC <- ggplot(datyr, aes(x = Date, y = FishBiomassChange, colour = Experiment)) +
+    geom_line() +
+    theme_bw() +
+    facet_grid(vars(Run), vars(Model), scales = "fixed"))
 ggsave("Figures/Fish_BiomassChange.pdf")
 

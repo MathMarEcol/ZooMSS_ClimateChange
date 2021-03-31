04e7132a3b2e0abe2df4d5a1067716e8acb7f076
jaseeverett <Jason.Everett@unsw.edu.au>
2021-03-31 14:50:17 +1100

    Start updating diet extraction

--- ZooMSS_CC_Code/ZooMSS_CC_2c_ExtractDiets.R
+++ ZooMSS_CC_Code/ZooMSS_CC_2c_ExtractDiets.R
@@ -32,6 +32,7 @@ for (r in 1:1){#length(runs)){
   zoo <- read_rds(paste0("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/",runs[r],"/Output/diets_",runs[r],".RDS"))
   mdl <- read_rds(paste0("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/",runs[r],"/Output/model_",runs[r],".RDS"))
 
+
   cellID <- ann(as.matrix(enviro_data[,c("sst", "chlo")]),
              as.matrix(nc[,c("SST", "Chl_log10")]),
              k = 1, verbose = FALSE)$knnIndexDist[,1]
@@ -79,6 +80,45 @@ for (r in 1:1){#length(runs)){
 
 
   # Convert it to a long tibble
+    # These next two lines are a hack to keep the size selection working for the moment.
+    # I will come up with a better solution soon
+    mdl2 <- mdl
+    mdl2$param$w <- mdl$param$w[minb:maxb]
+
+    Bio <- fZooMSS_SpeciesBiomass(fZooMSS_ExtractSizeRange(zoo, minb, maxb), mdl2)
+
+    Bio_df <- as_tibble(matrix(unlist(Bio), nrow=length(Bio), byrow=T), .name_repair = "unique") %>%
+      rename_with(~mdl$param$Groups$Species) %>%
+      mutate(cellID = 1:n()) %>% # Create a cellID
+      left_join(dplyr::select(enviro_data, cellID, chlo, sst), by = "cellID") %>%
+      rename(SST = sst, Chl = chlo) %>%
+      mutate(Chl_log10 = log10(Chl))
+
+    out <- ann(as.matrix(Bio_df[,c("SST", "Chl_log10")]),
+               as.matrix(nc[,c("SST", "Chl_log10")]),
+               k = 1, verbose = FALSE)
+
+    nc2 <- nc %>%
+      mutate(cellID = out$knnIndexDist[,1]) %>%
+      left_join(select(Bio_df, cellID, Flagellates:Fish_Large), by = "cellID") %>%
+      filter(!is.na(SST))
+
+    for (m in 1:length(models)){
+
+      nc_mdl <- nc2 %>%
+        filter(str_detect(Model, models[m]))
+
+      # temp folder for the moment while RDM is down
+      out_dir <- file.path("~","Dropbox","TempZooMSSData","")
+      write_rds(nc_mdl, paste0(out_dir, "ClimateChange_Compiled_withZooMSS_",models[m],"_",runs[r],".rds"))
+
+      # write_rds(nc_mdl, paste0(base_dir, "ClimateChange_Compiled_withZooMSS_",models[m],"_",runs[r],".rds"))
+      rm(nc_mdl)
+
+    }
+
+    rm(nc2, Bio, Bio_df, out)
+
 
 
 }

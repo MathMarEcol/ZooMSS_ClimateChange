4c8dc46f939c9668703795795abf433147ecf742
jaseeverett <Jason.Everett@unsw.edu.au>
2022-04-19 14:14:18 +1000

    Update diets for new data

--- ZooMSS_CC_2c_ExtractDiets.R
+++ ZooMSS_CC_2c_ExtractDiets.R
@@ -3,13 +3,16 @@ library(yaImpute)
 
 source("~/GitHub/ZooMSS/fZooMSS_Xtras.R")
 
-base_dir <- file.path("~","Nextcloud","MME2Work","ZooMSS_ClimateChange","")
 
-enviro_data <- read_rds("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/enviro_CMIP_Matrix_wPhyto.RDS")
+base_dir <- file.path("~","Nextcloud","MME2Work","ZooMSS","_LatestModel","20220315_TheMatrix2","")
+
+runs <- c("Control", "FixedCarbon")
+
+#### Load ZooMSS Matrix Data ####
+enviro_data <- read_rds("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20220315_TheMatrix2/Control/ClimateChange_Compiled_Distinct.rds") %>%
+  rename(cellID = FID)
+
 nc <- read_rds(paste0(base_dir, "ClimateChange_Compiled.rds"))
-
-# Setup vector of models and runs
-runs <- c("Control", "NoCarnivores", "NoOmnivores", "NoFilterFeeders", "OneZoo")
 
 models <- str_extract(unique(nc$Model), "[^-]+") # Extract model names
 exps <- unique(nc$Experiment)
@@ -17,10 +20,10 @@ exps <- unique(nc$Experiment)
 minb <- 1
 maxb <- 158 # Max weight of 100 kg.
 
-for (r in 2:length(runs)){
+for (r in 1:length(runs)){
 
-  zoo <- read_rds(paste0("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/",runs[r],"/Output/diets_",runs[r],".RDS")) # Run specific model output
-  mdl <- read_rds(paste0("~/Nextcloud/MME2Work/ZooMSS/_LatestModel/20200917_CMIP_Matrix/",runs[r],"/Output/model_",runs[r],".RDS")) # Model details
+  zoo <- read_rds(file.path(base_dir,runs[r],"Output",paste0("diets_",runs[r],".RDS")))
+  mdl <- read_rds(file.path(base_dir,runs[r],"Output",paste0("model_",runs[r],".RDS")))
 
   for (m in 1:length(models)){
     for (ex in 1:length(exps)){
@@ -32,7 +35,7 @@ for (r in 2:length(runs)){
 
       # Get cellID for enviro data for the climate runs
       cellID <- ann(as.matrix(enviro_data[,c("sst", "chlo")]),
-                    as.matrix(nc_mex[,c("SST", "Chl")]),
+                    as.matrix(nc_mex[,c("SST", "chlo")]),
                     k = 1, verbose = FALSE)$knnIndexDist[,1]
 
       zoo_mex <- zoo[cellID]
@@ -42,8 +45,7 @@ for (r in 2:length(runs)){
       # profvis::profvis({out <- map_df(.x = zoo_mex, mdl = mdl,  .f = fZooMS_MakeDietTibble)})
       # out <- map_df(.x = zoo_mex, mdl = mdl,  .f = fZooMS_MakeDietTibble)
 
-      write_rds(zoo_mex, file.path(base_dir, "Diets", runs[r], paste0("ClimateChange_withZooMSS_Diets_",exps[ex],"_", models[m],"_",runs[r],".rds")))
-
+      write_rds(nc_mex, paste0(file.path(base_dir,"Diets_"), "ClimateChange_Compiled_withZooMSS_",models[m],"_",runs[r],".rds"))
       # if (r == 1){
       #   write_rds(nc_mex, file.path(base_dir, "Diets", "EnviroData", paste0("EnviroData_Diets_",exps[ex],"_", models[m],".rds")))
       # }
